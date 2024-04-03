af9e25924d99e9d6671ea0cb618d4f27c4534313
Ricardo C. Monteiro <51727753+C4n2h4@users.noreply.github.com>
2024-04-03 19:42:58 -0300

    acrescentando rotas e controles
    criando comando para acrescentar rotas e controles

--- chatgpt-clone/server/src/controllers/prompt-controller.js
+++ chatgpt-clone/server/src/controllers/prompt-controller.js
@@ -1,11 +1,13 @@
-const OpenAIHelper = require ("openai")
+const InputPrompt = require("../config/openai")
+const openai = require ("openai")
 
 module.exports = {
-    async this.sendText(req, res){
+    async sendText(req, res){
         const openaiAPI = openai.configuration()
+        const inputModel = new InputPrompt (req.body)
         try{
             const response = await openaiAPI.createCompletion(
-                openai.textCompletion("me de nomes de artigos de node")
+                openai.textCompletion(inputModel)
             )
             return res.status(200).json({sucess:true, data: response.data.choices[0].text})
         } catch (error){
