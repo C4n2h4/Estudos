c6a5abdb9bd00907b1096af9727f9af6bdccf974
Ricardo C. Monteiro <51727753+C4n2h4@users.noreply.github.com>
2024-04-04 12:51:35 -0300

    modificando o css
    alterações no componente SideMenu.css
    alteração do modo de visibilidade

--- chatgpt-clone/web/src/App.js
+++ chatgpt-clone/web/src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+import{useState} from 'react'
+
 import './styles/App.css';
+import './styles/reset.css'
+import { SideMenu } from './components/SideMenu/SideMenu';
+import { makeRequest } from './api/api';
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <SideMenu></SideMenu>
+      <h1>App Works!</h1>
     </div>
   );
 }
