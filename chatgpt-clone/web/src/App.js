42fab354e7e440bb9af04b9f7883902af172239d
Ricardo C. Monteiro <51727753+C4n2h4@users.noreply.github.com>
2024-04-04 14:39:27 -0300

    Modificando o Input
    Modificando o Input

--- chatgpt-clone/web/src/App.js
+++ chatgpt-clone/web/src/App.js
@@ -4,14 +4,40 @@ import './styles/App.css';
 import './styles/reset.css'
 import { SideMenu } from './components/SideMenu/SideMenu';
 import { makeRequest } from './api/api';
+import { ChatMessage } from './components/ChatMessage/ChatMessage';
 
 
 
 function App() {
+
+  const[input, setInput] =useState("")
+  const[chatlog, setChatlog] =useState([{
+    user:"gpt"
+    message:"Como posso te ajudar hoje?"
+  }])
+
+    async function handleSubmit(e){
+        
+    }
+
   return (
     <div className="App">
+
       <SideMenu></SideMenu>
-      <h1>App Works!</h1>
+
+      <section className='chatbox'>
+        <div classeName='chat-log'>
+          {cahtlog.map((message, index)=>(<ChatMessage key={index} message={message}/>))}
+        </div>
+        <div className='chat-input-holder'>
+          <form onSubmit={handleSubmit}>
+            <input rows='1' className='chat-input-textarea' value={input} onChange={e =>setInput(e.target.value)}>
+            </input>
+
+          </form>
+        </div>
+      </section>
+
     </div>
   );
 }
